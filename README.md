fe58ef0753d3c2370a4022e1006d2bdae746dbe0
Ng Yik Phang <ngyikp@gmail.com>
2026-07-13 20:51:29 +0100

    Adjust web scaffold to my standards

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
 # UndertaleModTool in WASM
 
+## Development
+
+[Install .NET 10.0 SDK](https://dotnet.microsoft.com/en-us/download).
+
+Install the .NET workload:
+
 ```sh
 dotnet workload install wasm-tools
+```
 
-dotnet publish -c Release src/
-dotnet run --project src/
+Compile the .NET app:
+
+```sh
+rm -rf web/public/dotnet/
+dotnet publish -c Debug src/
+```
+
+Then run the React web app:
+
+```sh
+cd web
+npm install
+npm run dev
 ```
--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,22 +1,25 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import {defineConfig} from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-])
+export default defineConfig(
+	js.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	reactHooks.configs.flat.recommended,
+	reactRefresh.configs.vite,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+	},
+	{
+		files: ['**/*.{js,ts,tsx}'],
+	},
+	{
+		ignores: ['dist', 'public/dotnet/'],
+	},
+);
