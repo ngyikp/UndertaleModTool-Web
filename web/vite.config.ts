827b58cb273453a11a7bb20ed610b4938a0cdd0a
Ng Yik Phang <ngyikp@gmail.com>
2026-07-15 15:33:11 +0100

    Add Content Security Policy + security headers
    It's easier to be strict first and loosen the restrictions when
    needed, rather than bolting on security last

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -13,4 +13,36 @@ export default defineConfig({
 		react(),
 		babel({presets: [reactCompilerPreset()]}),
 	],
+	server: {
+		headers: getServerHeaders(),
+	},
 });
+
+function getServerHeaders() {
+	// Keep this in sync with /public/_headers
+	return {
+		'X-Content-Type-Options': 'nosniff',
+		'X-XSS-Protection': '0',
+		'X-Frame-Options': 'DENY',
+		// 'Strict-Transport-Security': 'max-age=31536000', // irrelevant during dev
+		'Cross-Origin-Embedder-Policy': 'require-corp',
+		'Cross-Origin-Opener-Policy': 'same-origin',
+		'Cross-Origin-Resource-Policy': 'same-origin',
+		'Permissions-Policy':
+			'accelerometer=(),bluetooth=(),camera=(),display-capture=(),geolocation=(),gyroscope=(),hid=(),magnetometer=(),microphone=(),midi=(),otp-credentials=(),payment=(),publickey-credentials-create=(),publickey-credentials-get=(),serial=(),usb=(),xr-spatial-tracking=()',
+
+		'Content-Security-Policy': [
+			"default-src 'self'",
+
+			// unsafe-inline: For Vite dev server
+			"script-src 'self' 'unsafe-inline' 'wasm-unsafe-eval'",
+
+			// unsafe-inline: For Vite dev
+			"style-src 'self' 'unsafe-inline'",
+
+			"frame-ancestors 'none'",
+			"base-uri 'none'",
+			"object-src 'none'",
+		].join(';'),
+	};
+}
