8f48fbeaf5542bd15318ed5a739b47638a75d401
Ng Yik Phang <ngyikp@gmail.com>
2026-07-20 00:30:03 +0100

    Add <seealso cref=""/> docs

--- src/Serializers/GameInfo.cs
+++ src/Serializers/GameInfo.cs
@@ -5,49 +5,107 @@ namespace Serializers
 {
     public record GameInfo
     {
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Name"/>
         public string? ProjectName { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.DisplayName"/>
         public string? DisplayName { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.IsGameMaker2()"/>
         public bool IsGameMaker2 { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.IsYYC()"/>
         public bool IsYYC { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.IsDebuggerDisabled"/>
         public bool IsDebuggerDisabled { get; set; }
+
         public required GameInfoVersion Version { get; set; }
 
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.BytecodeVersion"/>
         public int BytecodeVersion { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Config"/>
         public string? ConfigurationName { get; set; }
+
         public required GameInfoItemCounts ItemCounts { get; set; }
     }
 
     public record GameInfoVersion
     {
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Major"/>
         public uint Major { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Minor"/>
         public uint Minor { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Release"/>
         public uint Release { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleGeneralInfo.Build"/>
         public uint Build { get; set; }
     }
 
     public record GameInfoItemCounts
     {
+        /// <seealso cref="UndertaleModLib.UndertaleData.Sounds"/>
         public int Sounds { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Sprites"/>
         public int Sprites { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Backgrounds"/>
         public int Backgrounds { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Paths"/>
         public int Paths { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Scripts"/>
         public int Scripts { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Shaders"/>
         public int Shaders { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Fonts"/>
         public int Fonts { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Timelines"/>
         public int Timelines { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.GameObjects"/>
         public int GameObjects { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Rooms"/>
         public int Rooms { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Extensions"/>
         public int Extensions { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.TexturePageItems"/>
         public int TexturePageItems { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.TextureGroupInfo"/>
         public int TextureGroupInfo { get; set; }
 
+        /// <seealso cref="UndertaleModLib.UndertaleData.Code"/>
         public int Code { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Variables"/>
         public int Variables { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.Functions"/>
         public int Functions { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.CodeLocals"/>
         public int CodeLocals { get; set; }
 
+        /// <seealso cref="UndertaleModLib.UndertaleData.Strings"/>
         public int Strings { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.EmbeddedTextures"/>
         public int EmbeddedTextures { get; set; }
+
+        /// <seealso cref="UndertaleModLib.UndertaleData.EmbeddedAudio"/>
         public int EmbeddedAudio { get; set; }
     }
 
--- src/Serializers/GetCodeInfoByName.cs
+++ src/Serializers/GetCodeInfoByName.cs
@@ -5,6 +5,8 @@ namespace Serializers
     public record CodeInfo
     {
         public string? DecompiledCode { get; set; }
+
+        /// <seealso cref="UndertaleModLib.Models.UndertaleCode.ParentEntry"/>
         public string? ParentEntryName { get; set; }
     }
 
