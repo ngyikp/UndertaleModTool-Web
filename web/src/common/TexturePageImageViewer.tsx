43d5f308ab02ef7c07b57944ccef9ea8c19a626f
Ng Yik Phang <ngyikp@gmail.com>
2026-07-28 15:19:45 +0100

    Split drawTexturePageImage

--- web/src/common/TexturePageImageViewer.tsx
+++ web/src/common/TexturePageImageViewer.tsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from 'react';
 import {embeddedTexturesByIdQueryOptions} from '../messages/getEmbeddedTextureInfoById';
 import {texturePageByIdQueryOptions} from '../messages/getTexturePageInfoById';
 
-import drawImageToBlob from './drawImageToBlob';
+import drawTexturePageImage from './image/drawTexturePageImage';
 import ImageViewer from './ImageViewer';
 
 type Props = Readonly<{
@@ -19,91 +19,19 @@ export default function TexturePageImageViewer({texturePageId}: Props) {
 		embeddedTexturesByIdQueryOptions(texturePageData.EmbeddedTextureID),
 	);
 
+	const [blob, setBlob] = useState<Blob | null>(null);
 	const [error, setError] = useState<Error | null>(null);
-	const [finalBlob, setFinalBlob] = useState<Blob | null>(null);
 
 	useEffect(() => {
-		// todo add includePadding parameter
-		async function draw() {
-			setError(null);
-
-			// DDS is not supported yet
-			if (embeddedTextureData.Format === 'Dds') {
-				return;
-			}
-
-			// First, convert unusable file format to canvas
-			// todo this should probably be cached
-			const embeddedTextureCanvas = await drawImageToBlob(
-				embeddedTextureData.DownloadableFileContents,
-				embeddedTextureData.Bgra,
-				embeddedTextureData.Width,
-				embeddedTextureData.Height,
-			);
-
-			// Cropped image canvas
-			const canvas = document.createElement('canvas');
-			canvas.width = texturePageData.SourceWidth;
-			canvas.height = texturePageData.SourceHeight;
-
-			const ctx = canvas.getContext('2d', {alpha: true});
-			if (!ctx) {
-				throw new Error('Canvas rendering context is missing');
-			}
-
-			// Based on https://github.com/UnderminersTeam/UndertaleModTool/blob/2b6fe69722cec25219f1ae21f8111907c2a15629/UndertaleModLib/Util/TextureWorker.cs#L63
-			// Create an image cropped from the item's part of the texture page
-			const imageBitmap = await createImageBitmap(
-				embeddedTextureCanvas,
-				texturePageData.SourceX,
-				texturePageData.SourceY,
-				texturePageData.SourceWidth,
-				texturePageData.SourceHeight,
-			);
-
-			ctx.drawImage(imageBitmap, 0, 0);
-
-			// Resize the image, if necessary
-			if (
-				texturePageData.SourceWidth !== texturePageData.TargetWidth ||
-				texturePageData.SourceHeight !== texturePageData.TargetHeight
-			) {
-				const resizeWidth = texturePageData.TargetWidth;
-				const resizeHeight = texturePageData.TargetHeight;
-				if (canvas.width !== resizeWidth || canvas.height !== resizeHeight) {
-					ctx.drawImage(imageBitmap, 0, 0, resizeWidth, resizeHeight);
-				}
-			}
-
-			canvas.toBlob((croppedImageBlob) => {
-				if (croppedImageBlob == null) {
-					setError(new Error('Failed to render canvas image'));
-					return;
-				}
-
-				setFinalBlob(croppedImageBlob);
-			});
-		}
-
-		draw().catch(setError);
+		drawTexturePageImage(texturePageData, embeddedTextureData)
+			.then(setBlob)
+			.catch(setError);
 
 		return () => {
+			setBlob(null);
 			setError(null);
-			setFinalBlob(null);
 		};
-	}, [
-		embeddedTextureData.DownloadableFileContents,
-		embeddedTextureData.Bgra,
-		embeddedTextureData.Format,
-		embeddedTextureData.Width,
-		embeddedTextureData.Height,
-		texturePageData.SourceHeight,
-		texturePageData.SourceWidth,
-		texturePageData.SourceX,
-		texturePageData.SourceY,
-		texturePageData.TargetHeight,
-		texturePageData.TargetWidth,
-	]);
+	}, [embeddedTextureData, texturePageData]);
 
 	if (error) {
 		throw error;
@@ -111,7 +39,7 @@ export default function TexturePageImageViewer({texturePageId}: Props) {
 
 	return (
 		<ImageViewer
-			blob={finalBlob}
+			blob={blob}
 			fileName={'Texture ' + texturePageId.toString()}
 			width={texturePageData.TargetWidth}
 			height={texturePageData.TargetHeight}
---
+++ web/src/common/image/drawTexturePageImage.ts
@@ -0,0 +1,67 @@
+import type {EmbeddedTextureInfoType} from '../../messages/getEmbeddedTextureInfoById';
+import type {TexturePageInfoType} from '../../messages/getTexturePageInfoById';
+import drawImageToBlob from '../drawImageToBlob';
+
+// todo add includePadding parameter
+export default async function drawTexturePageImage(
+	texturePageData: TexturePageInfoType,
+	embeddedTextureData: EmbeddedTextureInfoType,
+): Promise<Blob> {
+	if (embeddedTextureData.Format === 'Dds') {
+		throw new Error('DDS format is not supported yet');
+	}
+
+	// First, convert unusable file format to canvas
+	// todo this should probably be cached
+	const embeddedTextureCanvas = await drawImageToBlob(
+		embeddedTextureData.DownloadableFileContents,
+		embeddedTextureData.Bgra,
+		embeddedTextureData.Width,
+		embeddedTextureData.Height,
+	);
+
+	// Cropped image canvas
+	const canvas = document.createElement('canvas');
+	canvas.width = texturePageData.SourceWidth;
+	canvas.height = texturePageData.SourceHeight;
+
+	const ctx = canvas.getContext('2d', {alpha: true});
+	if (!ctx) {
+		throw new Error('Canvas rendering context is missing');
+	}
+
+	// Based on https://github.com/UnderminersTeam/UndertaleModTool/blob/2b6fe69722cec25219f1ae21f8111907c2a15629/UndertaleModLib/Util/TextureWorker.cs#L63
+	// Create an image cropped from the item's part of the texture page
+	const imageBitmap = await createImageBitmap(
+		embeddedTextureCanvas,
+		texturePageData.SourceX,
+		texturePageData.SourceY,
+		texturePageData.SourceWidth,
+		texturePageData.SourceHeight,
+	);
+
+	ctx.drawImage(imageBitmap, 0, 0);
+
+	// Resize the image, if necessary
+	if (
+		texturePageData.SourceWidth !== texturePageData.TargetWidth ||
+		texturePageData.SourceHeight !== texturePageData.TargetHeight
+	) {
+		const resizeWidth = texturePageData.TargetWidth;
+		const resizeHeight = texturePageData.TargetHeight;
+		if (canvas.width !== resizeWidth || canvas.height !== resizeHeight) {
+			ctx.drawImage(imageBitmap, 0, 0, resizeWidth, resizeHeight);
+		}
+	}
+
+	return new Promise((resolve, reject) => {
+		canvas.toBlob((croppedImageBlob) => {
+			if (croppedImageBlob == null) {
+				reject(new Error('Failed to render canvas image'));
+				return;
+			}
+
+			resolve(croppedImageBlob);
+		});
+	});
+}
