4d167ab48ba13286da1ca2e035c148f6ed94907c
Ng Yik Phang <ngyikp@gmail.com>
2026-07-28 13:49:34 +0100

    Split texture page image to its own component
    To be reused for sprites

---
+++ web/src/common/TexturePageImageViewer.tsx
@@ -0,0 +1,121 @@
+import {useSuspenseQuery} from '@tanstack/react-query';
+import {useEffect, useState} from 'react';
+
+import {embeddedTexturesByIdQueryOptions} from '../messages/getEmbeddedTextureInfoById';
+import {texturePageByIdQueryOptions} from '../messages/getTexturePageInfoById';
+
+import drawImageToBlob from './drawImageToBlob';
+import ImageViewer from './ImageViewer';
+
+type Props = Readonly<{
+	texturePageId: number;
+}>;
+
+export default function TexturePageImageViewer({texturePageId}: Props) {
+	const {data: texturePageData} = useSuspenseQuery(
+		texturePageByIdQueryOptions(texturePageId),
+	);
+	const {data: embeddedTextureData} = useSuspenseQuery(
+		embeddedTexturesByIdQueryOptions(texturePageData.EmbeddedTextureID),
+	);
+
+	const [error, setError] = useState<Error | null>(null);
+	const [finalBlob, setFinalBlob] = useState<Blob | null>(null);
+
+	useEffect(() => {
+		// todo add includePadding parameter
+		async function draw() {
+			setError(null);
+
+			// DDS is not supported yet
+			if (embeddedTextureData.Format === 'Dds') {
+				return;
+			}
+
+			// First, convert unusable file format to canvas
+			// todo this should probably be cached
+			const embeddedTextureCanvas = await drawImageToBlob(
+				embeddedTextureData.DownloadableFileContents,
+				embeddedTextureData.Bgra,
+				embeddedTextureData.Width,
+				embeddedTextureData.Height,
+			);
+
+			// Cropped image canvas
+			const canvas = document.createElement('canvas');
+			canvas.width = texturePageData.SourceWidth;
+			canvas.height = texturePageData.SourceHeight;
+
+			const ctx = canvas.getContext('2d', {alpha: true});
+			if (!ctx) {
+				throw new Error('Canvas rendering context is missing');
+			}
+
+			// Based on https://github.com/UnderminersTeam/UndertaleModTool/blob/2b6fe69722cec25219f1ae21f8111907c2a15629/UndertaleModLib/Util/TextureWorker.cs#L63
+			// Create an image cropped from the item's part of the texture page
+			const imageBitmap = await createImageBitmap(
+				embeddedTextureCanvas,
+				texturePageData.SourceX,
+				texturePageData.SourceY,
+				texturePageData.SourceWidth,
+				texturePageData.SourceHeight,
+			);
+
+			ctx.drawImage(imageBitmap, 0, 0);
+
+			// Resize the image, if necessary
+			if (
+				texturePageData.SourceWidth !== texturePageData.TargetWidth ||
+				texturePageData.SourceHeight !== texturePageData.TargetHeight
+			) {
+				const resizeWidth = texturePageData.TargetWidth;
+				const resizeHeight = texturePageData.TargetHeight;
+				if (canvas.width !== resizeWidth || canvas.height !== resizeHeight) {
+					ctx.drawImage(imageBitmap, 0, 0, resizeWidth, resizeHeight);
+				}
+			}
+
+			canvas.toBlob((croppedImageBlob) => {
+				if (croppedImageBlob == null) {
+					setError(new Error('Failed to render canvas image'));
+					return;
+				}
+
+				setFinalBlob(croppedImageBlob);
+			});
+		}
+
+		draw().catch(setError);
+
+		return () => {
+			setError(null);
+			setFinalBlob(null);
+		};
+	}, [
+		embeddedTextureData.DownloadableFileContents,
+		embeddedTextureData.Bgra,
+		embeddedTextureData.Format,
+		embeddedTextureData.Width,
+		embeddedTextureData.Height,
+		texturePageData.SourceHeight,
+		texturePageData.SourceWidth,
+		texturePageData.SourceX,
+		texturePageData.SourceY,
+		texturePageData.TargetHeight,
+		texturePageData.TargetWidth,
+	]);
+
+	if (error) {
+		throw error;
+	}
+
+	return finalBlob ? (
+		<ImageViewer
+			blob={finalBlob}
+			fileName={'Texture ' + texturePageId.toString()}
+			width={texturePageData.TargetWidth}
+			height={texturePageData.TargetHeight}
+			enableDownload={embeddedTextureData.DownloadableFileContents != null}
+		/>
+	) : null;
+}
--- web/src/routes/_app/texture-pages.$id.tsx
+++ web/src/routes/_app/texture-pages.$id.tsx
@@ -1,12 +1,10 @@
 import {Stack, Title} from '@mantine/core';
 import {useSuspenseQuery} from '@tanstack/react-query';
 import {createFileRoute, Link, useParams} from '@tanstack/react-router';
-import {useEffect, useState} from 'react';
 
 import BasicErrorAlert from '../../common/BasicErrorAlert';
 import DocumentTitle from '../../common/DocumentTitle';
-import drawImageToBlob from '../../common/drawImageToBlob';
-import ImageViewer from '../../common/ImageViewer';
+import TexturePageImageViewer from '../../common/TexturePageImageViewer';
 import {embeddedTexturesByIdQueryOptions} from '../../messages/getEmbeddedTextureInfoById';
 import {texturePageByIdQueryOptions} from '../../messages/getTexturePageInfoById';
 
@@ -19,103 +17,12 @@ function RouteComponent() {
 	const {data: texturePageData} = useSuspenseQuery(
 		texturePageByIdQueryOptions(id),
 	);
-	const {data: embeddedTextureData} = useSuspenseQuery(
-		embeddedTexturesByIdQueryOptions(texturePageData.EmbeddedTextureID),
-	);
-
-	const [error, setError] = useState<Error | null>(null);
-	const [finalBlob, setFinalBlob] = useState<Blob | null>(null);
-
-	useEffect(() => {
-		// todo add includePadding parameter
-		async function draw() {
-			setError(null);
-
-			// DDS is not supported yet
-			if (embeddedTextureData.Format === 'Dds') {
-				return;
-			}
-
-			// First, convert unusable file format to canvas
-			// todo this should probably be cached
-			const embeddedTextureCanvas = await drawImageToBlob(
-				embeddedTextureData.DownloadableFileContents,
-				embeddedTextureData.Bgra,
-				embeddedTextureData.Width,
-				embeddedTextureData.Height,
-			);
-
-			// Cropped image canvas
-			const canvas = document.createElement('canvas');
-			canvas.width = texturePageData.SourceWidth;
-			canvas.height = texturePageData.SourceHeight;
-
-			const ctx = canvas.getContext('2d', {alpha: true});
-			if (!ctx) {
-				throw new Error('Canvas rendering context is missing');
-			}
-
-			// Based on https://github.com/UnderminersTeam/UndertaleModTool/blob/2b6fe69722cec25219f1ae21f8111907c2a15629/UndertaleModLib/Util/TextureWorker.cs#L63
-			// Create an image cropped from the item's part of the texture page
-			const imageBitmap = await createImageBitmap(
-				embeddedTextureCanvas,
-				texturePageData.SourceX,
-				texturePageData.SourceY,
-				texturePageData.SourceWidth,
-				texturePageData.SourceHeight,
-			);
-
-			ctx.drawImage(imageBitmap, 0, 0);
-
-			// Resize the image, if necessary
-			if (
-				texturePageData.SourceWidth !== texturePageData.TargetWidth ||
-				texturePageData.SourceHeight !== texturePageData.TargetHeight
-			) {
-				const resizeWidth = texturePageData.TargetWidth;
-				const resizeHeight = texturePageData.TargetHeight;
-				if (canvas.width !== resizeWidth || canvas.height !== resizeHeight) {
-					ctx.drawImage(imageBitmap, 0, 0, resizeWidth, resizeHeight);
-				}
-			}
-
-			canvas.toBlob((croppedImageBlob) => {
-				if (croppedImageBlob == null) {
-					setError(new Error('Failed to render canvas image'));
-					return;
-				}
-
-				setFinalBlob(croppedImageBlob);
-			});
-		}
-
-		draw().catch(setError);
-
-		return () => {
-			setError(null);
-			setFinalBlob(null);
-		};
-	}, [
-		embeddedTextureData.DownloadableFileContents,
-		embeddedTextureData.Bgra,
-		embeddedTextureData.Format,
-		embeddedTextureData.Width,
-		embeddedTextureData.Height,
-		texturePageData.SourceHeight,
-		texturePageData.SourceWidth,
-		texturePageData.SourceX,
-		texturePageData.SourceY,
-		texturePageData.TargetHeight,
-		texturePageData.TargetWidth,
-	]);
 
 	return (
 		<Stack flex="1" mt="md" mb="lg" style={{minWidth: 0}}>
 			<DocumentTitle text={['Texture ' + id.toString(), 'Texture pages']} />
 
 			<Title order={2}>Texture {id}</Title>
-
-			{error != null ? <BasicErrorAlert error={error} /> : null}
 
 			<p>
 				Source position: {texturePageData.SourceX}x{texturePageData.SourceY}
@@ -145,15 +52,7 @@ function RouteComponent() {
 				</Link>
 			</p>
 
-			{finalBlob ? (
-				<ImageViewer
-					blob={finalBlob}
-					fileName={'Texture ' + id.toString()}
-					width={texturePageData.TargetWidth}
-					height={texturePageData.TargetHeight}
-					enableDownload={embeddedTextureData.DownloadableFileContents != null}
-				/>
-			) : null}
+			<TexturePageImageViewer key={id} texturePageId={id} />
 		</Stack>
 	);
 }
