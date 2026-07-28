7f02d6e615b1259a0c80559cc426f17a3880f568
Ng Yik Phang <ngyikp@gmail.com>
2026-07-28 14:23:51 +0100

    Add viewing sprites 🎉
    Still lots of tidying up to do...

--- web/src/routes/_app/sprites.$name.tsx
+++ web/src/routes/_app/sprites.$name.tsx
@@ -1,9 +1,11 @@
-import {Stack, Title} from '@mantine/core';
+import {Pagination, Stack, Title} from '@mantine/core';
 import {useSuspenseQuery} from '@tanstack/react-query';
 import {createFileRoute, useParams} from '@tanstack/react-router';
+import {useState} from 'react';
 
 import BasicErrorAlert from '../../common/BasicErrorAlert';
 import DocumentTitle from '../../common/DocumentTitle';
+import TexturePageImageViewer from '../../common/TexturePageImageViewer';
 import {spriteInfoByNameQueryOptions} from '../../messages/getSpriteInfoByName';
 import {ManagedErrorFromDotNet} from '../../worker/ManagedErrorFromDotNet';
 
@@ -14,13 +16,28 @@ function RouteComponent() {
 	});
 
 	const {data} = useSuspenseQuery(spriteInfoByNameQueryOptions(name));
-	console.log(data);
+
+	const [page, setPage] = useState(1);
+	const texturePage = data.TexturePageIDs[page - 1];
 
 	return (
 		<Stack flex="1" mt="md" mb="lg" style={{minWidth: 0}}>
 			<DocumentTitle text={[name, 'Sprites']} />
 
 			<Title order={2}>{name}</Title>
+
+			{texturePage ? (
+				<TexturePageImageViewer key={texturePage} texturePageId={texturePage} />
+			) : null}
+
+			{data.TexturePageIDs.length > 1 ? (
+				<Pagination
+					total={data.TexturePageIDs.length}
+					value={page}
+					onChange={setPage}
+					py="md"
+				/>
+			) : null}
 		</Stack>
 	);
 }
