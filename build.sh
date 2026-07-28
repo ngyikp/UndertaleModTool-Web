a496c468516fc3a5ee96753c204e6d30a135a09b
Ng Yik Phang <ngyikp@gmail.com>
2026-07-28 21:18:49 +0100

    Add Cloudflare Pages deploy script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# https://developers.cloudflare.com/pages/framework-guides/deploy-a-blazor-site/
+
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet
+./dotnet/dotnet --version
+./dotnet/dotnet workload install wasm-tools
+./dotnet/dotnet publish -c Release src/
+
+cd ./web
+npm ci
+npm run build
