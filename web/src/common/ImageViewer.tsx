55c9a9f548f3c57872cb1d2dbc56373a0b65b8a0
Ng Yik Phang <ngyikp@gmail.com>
2026-07-28 14:43:50 +0100

    Split ImageAppearanceSelect

--- web/src/common/ImageViewer.tsx
+++ web/src/common/ImageViewer.tsx
@@ -1,11 +1,11 @@
-import {Button, Group, Select} from '@mantine/core';
+import {Button, Group} from '@mantine/core';
 
 import {useDataStore} from '../data-store';
 
+import ImageAppearanceSelect from './image/ImageAppearanceSelect';
 import type {Appearance} from './image/ImageAppearanceType';
 import ImageWithPlaceholder from './image/ImageWithPlaceholder';
 import {useBlobAsUrl} from './image/useBlobAsUrl';
-import styles from './ImageViewer.module.css';
 
 export type ImageViewerSettings = {
 	appearance: Appearance;
@@ -27,29 +27,14 @@ export default function ImageViewer({
 	enableDownload,
 }: Props) {
 	const settings = useDataStore((state) => state.imageViewerSettings);
-	const setSettings = useDataStore((state) => state.setImageViewerSettings);
 
 	const blobUrl = useBlobAsUrl(blob);
 
 	return (
 		<>
-			<Group gap="xs">
-				Appearance:
-				<Select
-					data={[
-						{value: 'BLACK', label: 'Black'},
-						{value: 'WHITE', label: 'White'},
-						{value: 'CHECKERBOARD', label: 'Checkerboard'},
-					]}
-					value={settings.appearance}
-					onChange={(value) => {
-						setSettings({
-							...settings,
-							appearance: value ?? 'CHECKERBOARD',
-						});
-					}}
-					className={styles.appearanceSelect}
-				/>
+			<Group>
+				<ImageAppearanceSelect />
+
 				{enableDownload ? (
 					<Button
 						component="a"
---
+++ web/src/common/image/ImageAppearanceSelect.tsx
@@ -0,0 +1,31 @@
+import {Group, Select} from '@mantine/core';
+
+import {useDataStore} from '../../data-store';
+
+import styles from './ImageAppearanceSelect.module.css';
+
+export default function ImageAppearanceSelect() {
+	const settings = useDataStore((state) => state.imageViewerSettings);
+	const setSettings = useDataStore((state) => state.setImageViewerSettings);
+
+	return (
+		<Group gap="xs">
+			Appearance:
+			<Select
+				data={[
+					{value: 'BLACK', label: 'Black'},
+					{value: 'WHITE', label: 'White'},
+					{value: 'CHECKERBOARD', label: 'Checkerboard'},
+				]}
+				value={settings.appearance}
+				onChange={(value) => {
+					setSettings({
+						...settings,
+						appearance: value ?? 'CHECKERBOARD',
+					});
+				}}
+				className={styles.select}
+			/>
+		</Group>
+	);
+}
