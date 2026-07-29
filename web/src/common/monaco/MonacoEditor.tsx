21219d2e2d0c6704848657184880da5561c83779
Ng Yik Phang <ngyikp@gmail.com>
2026-07-29 21:04:14 +0100

    Disable sticky scroll on Monaco editor
    Might bring it back if it's less buggy

--- web/src/common/monaco/MonacoEditor.tsx
+++ web/src/common/monaco/MonacoEditor.tsx
@@ -34,6 +34,9 @@ export default function MonacoEditor({
 		if (!editorRef.current) {
 			editorRef.current = monaco.editor.create(divRef.current, {
 				language: 'gml',
+				stickyScroll: {
+					enabled: false,
+				},
 			});
 		}
 
