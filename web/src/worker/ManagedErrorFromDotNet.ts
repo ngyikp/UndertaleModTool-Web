8bd2d14747454920502504ac5769f05aa48fbd8e
Ng Yik Phang <ngyikp@gmail.com>
2026-08-05 16:45:01 +0100

    Consistent Typescript configs

--- web/src/worker/ManagedErrorFromDotNet.ts
+++ web/src/worker/ManagedErrorFromDotNet.ts
@@ -1,6 +1,6 @@
 // ManagedError: https://github.com/dotnet/runtime/blob/ea3f7f141e0596cab37785d305910e64d031ab29/src/mono/browser/runtime/marshal.ts#L397
 export class ManagedErrorFromDotNet extends Error {
-	stack: string;
+	override stack: string;
 
 	constructor(message: string, stack: string) {
 		super(message);
