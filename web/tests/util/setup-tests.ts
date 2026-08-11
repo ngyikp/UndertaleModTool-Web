edaf09c248cacad4fb6603e5fa02d6f7a9739ba0
Ng Yik Phang <ngyikp@gmail.com>
2026-08-11 22:33:23 +0100

    Ensure React is cleaned up after each test
    Vitest globals are not injected, so cleanup doesn't run, it's not
    noticeable right now since there is only one test 🙈

--- web/tests/util/setup-tests.ts
+++ web/tests/util/setup-tests.ts
@@ -1,4 +1,14 @@
 import '@testing-library/jest-dom/vitest';
-import {configure as configureTestingLibrary} from '@testing-library/react';
+import {
+	// eslint-disable-next-line testing-library/no-manual-cleanup
+	cleanup as cleanupTestingLibrary,
+	configure as configureTestingLibrary,
+} from '@testing-library/react';
+import {afterEach} from 'vitest';
 
 configureTestingLibrary({reactStrictMode: true});
+
+// https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
+afterEach(() => {
+	cleanupTestingLibrary();
+});
