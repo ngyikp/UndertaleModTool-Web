d3a8c8022d5540e0a670af05000cd71796fce586
Ng Yik Phang <ngyikp@gmail.com>
2026-09-17 12:47:58 +0100

    Fix confusing main page when data is already load
    1. Open 2 browser tabs
    2. Load a data file on tab 1
    3. Go to main page on tab 2
    
    This also means the SharedWorker will run on main page, since we
    need it to know if a data file is already loaded
    
    Ref https://github.com/ngyikp/UndertaleModTool-Web/issues/8

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,11 +1,18 @@
-import {createFileRoute, useNavigate} from '@tanstack/react-router';
+import {useQuery} from '@tanstack/react-query';
+import {createFileRoute, Navigate, useNavigate} from '@tanstack/react-router';
 
 import DataFileInput from '../common/DataFileInput';
 import DocumentTitle from '../common/DocumentTitle';
 import GenericHeaderAndFooter from '../common/GenericHeaderAndFooter';
+import {getDataFileLoadInfoQueryOptions} from '../messages/getDataFileLoadInfo';
 
 function Index() {
 	const navigate = useNavigate({from: '/'});
+
+	const {data: dataFileLoadInfo} = useQuery(getDataFileLoadInfoQueryOptions());
+	if (dataFileLoadInfo?.Successful) {
+		return <Navigate from="/" to="/general-info" mask={{to: '/'}} />;
+	}
 
 	return (
 		<GenericHeaderAndFooter>
